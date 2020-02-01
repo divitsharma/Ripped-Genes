746cd68c86b8ae8b849832f96af7786f026fa36d
MEPC\Divit <dsharma5095@gmail.com>
2020-02-01 04:36:18 -0500

    two strands

---
+++ Assets/Scripts/HelixCurve.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelixCurve : MonoBehaviour
+{
+    public float zr;
+    public float yr;
+    public float c;
+
+    public float x(float t)
+    {
+        return c * t;
+    }
+
+    public float y(float t)
+    {
+        return yr * Mathf.Cos(t);
+    }
+
+    public float z(float t)
+    {
+        return zr * Mathf.Sin(t);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        float twopi = 3 * Mathf.PI;
+
+        float zoffset = zr;
+
+        for (float t = 0; t < twopi; t += 0.02f)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawSphere(new Vector3(x(t), y(t), z(t)), 0.1f);
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(new Vector3(x(t), y(t + Mathf.PI), z(t + Mathf.PI)), 0.1f);
+        }
+    }
+}
---
+++ Assets/Scripts/HelixRenderer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class HelixRenderer : MonoBehaviour
+{
+    HelixCurve curve;
+    public TubeRenderer tubeRendererBlue;
+    public TubeRenderer tubeRendererRed;
+
+    void Start()
+    {
+        curve = GameObject.FindObjectOfType<HelixCurve>();
+
+        RenderHelix();
+    }
+
+    void RenderHelix()
+    {
+        float length = 3 * Mathf.PI;
+        float step = 0.05f;
+        int npos = (int)(length / step);
+        Vector3[] positionsBlue = new Vector3[npos];
+        Vector3[] positionsRed = new Vector3[npos];
+
+        for (int i = 0; i < npos; i++)
+        {
+            float t = step * i;
+            positionsBlue[i] = new Vector3(curve.x(t), curve.y(t), curve.z(t));
+            positionsRed[i] = new Vector3(curve.x(t), curve.y(t + Mathf.PI), curve.z(t + Mathf.PI));
+        }
+
+        tubeRendererBlue.SetPositions(positionsBlue);
+        tubeRendererRed.SetPositions(positionsRed);
+    }
+
+}
